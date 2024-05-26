591fb7b9c8f0aeede3d63ac9ee8badccbc60dfac
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2024-05-26 18:28:58 +0800

    Worked on the folders (now called notebooks).
    - Add settings button
    - Polished visuals. Active tabs now move up, clear button now has hover and click feedback.
    - Added "Add Notebook" button
    - Started to implement notebook system's foundation. (Create and open notebooks). It's still not working as I'll need to rewrite the scripts for tabs to fully implement it.
    
    You should see commented out scripts and html files as it's not working properly yet.

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 
 var addTab = document.getElementById("add-tab-btn");
+var addNotebook = document.getElementById("add-notebook-btn");
+
 var container = document.getElementById("container");
 
 var notepads = document.getElementsByClassName("notepad");
 var tabs = document.getElementsByClassName("tab-box");
 
+var notebookSection = document.getElementById('notebooks-section');
+
 var tabCounter = 1;
+var notebookCounter = 1;
+
 var tabsData = [];
+var notebooksData = []; // Notebooks will be an array of tabs/notepads
 
 var openedTab;
 var openedNotepad;
+var openedNotebook;
 
 var notepadDefaultText = "Type here...";
 
@@ -26,20 +34,21 @@ clearButton.addEventListener("click", function() {
 
 function clearAllTabs(){
     localStorage.clear();
+    location.reload();
 }
 
-function createTab ()
+function createTab () //creates a new tab
 {
-    var tabID = 'tab-' + tabCounter;
-    var tabName = 'New tab ' + tabCounter;
+    var tab_id = 'tab-' + tabCounter;
+    var tab_name = 'New tab ' + tabCounter;
     // This creates the Tab button
-    var tab = document.createElement("div");
-    tab.id = tabID;
+    var tab_ = document.createElement("div");
+    tab_.id = tab_id;
     var span = document.createElement("span");
-    tab.appendChild(span);
-    span.textContent = tabName;
-    tab.className = 'tab-box';
-    document.getElementById("tabs-section").insertBefore(tab, addTab);
+    tab_.appendChild(span);
+    span.textContent = tab_name;
+    tab_.className = 'tab-box';
+    document.getElementById("tabs-section").insertBefore(tab_, addTab);
 
     // This creates the Tab window
 
@@ -52,67 +61,72 @@ function createTab ()
     container.appendChild(tabWindow);
 
     //This will make the tab clickable 
-    tab.addEventListener("click", function() {
-        openTab(tabWindow.id, tabID);
+    tab_.addEventListener("click", function() {
+        openTab(tabWindow.id, tab_id);
     });
 
-    tab.addEventListener('dblclick', function() {
-        renameTab(tab, span);
+    tab_.addEventListener('dblclick', function() {
+        renameTab(tab_, span);
     });
     // adds the save function that will be called when the text content changes
     tabWindow.addEventListener('input', function() {
-        saveContent(tabID, tabWindow.value);
+        saveContent(tab_id, tabWindow.value);
     })
 
     //#region Close button
     // This creates the close button and make it functional
     var closeButton = document.createElement('img');
     
-    closeButton.src = 'https://img.icons8.com/?size=100&id=2i5n7zNvArOt&format=png&color=000000';
     closeButton.className = 'close-btn';
     
-    tab.appendChild(closeButton);
+    tab_.appendChild(closeButton);
 
     closeButton.addEventListener('click', function(event) {
         event.stopPropagation();
-        closeTab(tabWindow.id, tab.id);
+        closeTab(tabWindow.id, tab_.id);
     });
     //#endregion
 
-    var tabData = {
-        id: tabID,
-        tabName: tabName,
+    var tab_data = {
+        id: tab_id,
+        tabName: tab_name,
         text: tabWindow.value
     };
     
-    tabsData.push(tabData);
+    tabsData.push(tab_data);
     saveTabs();
     console.log('Created a tab id ' + tabWindow.id + ' with class name ' + tabWindow.className)
     
     tabCounter++;
-    openTab(tabWindow.id, tabID);
+    openTab(tabWindow.id, tab_id);
+
+    notebooksData.tabsData?.push(tab_data);// Inserts the tab to the notebook
+
 }
-function openTab(notepadID, tabID) {
+function openTab(notepad_id, tab_id) { //opens a tab
     for (let i = 0; i < notepads.length; i++) {
         notepads[i].style.display = "none";
     }
-    document.getElementById(notepadID).style.display = "block";
+    document.getElementById(notepad_id).style.display = "block";
     
     if(openedTab != null)
     {
         document.getElementById(openedTab).style.filter = 'saturate(250%)';
         document.getElementById(openedTab).style.opacity = '.9';
-        document.getElementById(openedTab).style.height = '80%';
-    }
-    openedTab = tabID;
-    openedNotepad = notepadID;
+        document.getElementById(openedTab).style.height = 'fit-content';
+        document.getElementById(openedTab).style.transform = 'translateY(0px)';
 
-    document.getElementById(tabID).style.filter = 'brightness(110%) saturate(250%)';
-    document.getElementById(tabID).style.height = '93%';
+    }
+    openedTab = tab_id;
+    openedNotepad = notepad_id;
+
+    document.getElementById(tab_id).style.filter = 'brightness(110%) saturate(250%)';
+    document.getElementById(tab_id).style.height = '90%';
+    document.getElementById(tab_id).style.transform = 'translateY(-5px)';
 }
 
-function renameTab (tab) {
-    var currentName = tab.firstChild.textContent;
+function renameTab (tab_) { // this renames the tab
+    var currentName_ = tab_.firstChild.textContent;
     // var newName = prompt('Enter tab name');
     // if(newName){
     // tab.textContent = newName;
@@ -120,59 +134,96 @@ function renameTab (tab) {
 
     // console.log('Renamed ' + currentName + ' to ' + newName);
     
-    var input = document.createElement('input');
-    input.type = 'text';
-    input.value = currentName;
-    input.className = 'rename-input';
-    input.maxLength = 30;
-    input.style.flexBasis = '100';
-    tab.firstChild.replaceWith(input);
-    input.focus();
+    var input_ = document.createElement('input');
+    input_.type = 'text';
+    input_.value = currentName_;
+    input_.className = 'rename-input';
+    input_.maxLength = 30;
+    input_.style.flexBasis = '100';
+    tab_.firstChild.replaceWith(input_);
+    input_.focus();
 
-    input.addEventListener("blur", function(){
-        var newName = input.value;
-        var span;
-        span = document.createElement('span');
+    input_.addEventListener("blur", function(){
+        var new_name = input_.value;
+        var span_;
+        span_ = document.createElement('span');
         
 
-        if(newName.trim() !== ''){
+        if(new_name.trim() !== ''){
             
-            span.textContent = newName;
-            input.replaceWith(span);
+            span_.textContent = new_name;
+            input_.replaceWith(span_);
 
-            var tabData = tabsData.find(tabData => tabData.id === tab.id);
+            var tab_data = tabsData.find(tabData => tabData.id === tab_.id);
             
-                tabData.tabName = newName;
+                tab_data.tabName = new_name;
                 saveTabs();
 
-                console.log('Changed ' + tab.id + ' from ' + currentName + 'to ' + tab.tabName)
+                console.log('Changed ' + tab_.id + ' from ' + currentName_ + 'to ' + tab_.tabName)
                 console.log(tabsData);
         } else {
-            span.textContent = currentName;
-            input.replaceWith(span);
+            span_.textContent = currentName_;
+            input_.replaceWith(span_);
         }
     });
-    input.addEventListener('keydown', function(event){
-        var key = event.key;
-        if(key === 'Enter'){
+    input_.addEventListener('keydown', function(event){
+        var key_ = event.key;
+        if(key_ === 'Enter'){
             event.preventDefault();
-            input.blur();
+            input_.blur();
         }
     });
 }
 
 function saveTabs(){ // this will save all the tabs
     localStorage.setItem("savedTabs", JSON.stringify(tabsData));
+    localStorage.setItem("Tab Counter", tabCounter);
 }
-function saveContent(tabID, content) { // saves the specified notepad's text contents
-    var tab = tabsData.find(tab => tab.id == tabID);
-    tab.text = content;
+function saveContent(tab_id, content_) { // saves the specified notepad's text contents
+    var tab = tabsData.find(tab => tab.id == tab_id);
+    tab.text = content_;
 
     saveTabs();
 }
 
-function load()
-{
+function closeTab(notepad_id, tab_id) {
+    var tabElement = document.getElementById(tab_id);
+    console.log('closed ' + notepad_id + ' ' + tab_id);
+    if (tabElement){
+        tabElement.remove();
+    }
+
+    var notepadElement = document.getElementById(notepad_id);
+
+    if (notepadElement)
+        notepadElement.remove();
+
+    tabsData = tabsData.filter(tab => tab.id !== tab_id);
+    saveTabs();
+
+    if(openedTab === tab_id)
+    {
+        openedTab = null;
+        notepad_id = null;
+    }
+
+    // Open the last tab if any
+    // // if (tabsData.length > 0) {
+    // //     var lastTab = tabsData[tabsData.length - 1];
+    // //     openTab(lastTab.notepadID, lastTab.tabID);
+    // // } else {
+    // //     openedTab = null;
+    // //     openedNotepad = null;
+    // // }
+
+    // openTab(openedNotepad, openedTab);
+}
+
+function load() // loads all the tabs
+{  
+
+    // tabCounter = localStorage.getItem("Tab Counter");
+
     var data = JSON.parse(localStorage.getItem("savedTabs"));
 
     if(Array.isArray(data)){
@@ -194,7 +245,6 @@ function load()
             // This creates the close button and make it functional
             var closeButton = document.createElement('img');
             
-            closeButton.src = 'https://img.icons8.com/?size=100&id=2i5n7zNvArOt&format=png&color=000000';
             closeButton.className = 'close-btn';
             
             tab.appendChild(closeButton);
@@ -237,39 +287,40 @@ function load()
     }  
 }
 
-function closeTab(notepadID, tabID) {
-    var tabElement = document.getElementById(tabID);
-    console.log('closed ' + notepadID + ' ' + tabID);
-    if (tabElement){
-        tabElement.remove();
-    }
+addNotebook?.addEventListener("click", function(){
+    createNotebook();
+})
+createNotebook();
+function createNotebook() { // This will create the UI for the notebook
+    var notebook_name = 'Notebook ' + notebookCounter;
+    var notebook_id = 'notebookID-' + notebookCounter;
+    var notebook_data = {
+        name: notebook_name,
+        id: notebook_id,
+        tabsData: []
+    };
 
-    var notepadElement = document.getElementById(notepadID);
+    notebooksData.push(notebook_data);
 
-    if (notepadElement)
-        notepadElement.remove();
+    var notebook_ = document.createElement('div');
+    notebook_.textContent = notebook_name;
+    // var input_ = document.createElement('input');
+    // input_.className = 'notebook-rename';
+    // notebook_.firstChild.replaceWith(input_);
+    // input_.focus();
+    // notebook_.textContent = input_.value;
+    // notebook_.id = notebook_id;
 
-    tabsData = tabsData.filter(tab => tab.id !== tabID);
-    saveTabs();
+    notebook_.className = 'notebook';
 
-    if(openedTab === tabID)
-    {
-        openedTab = null;
-        notepadID = null;
-    }
-
-    // Open the last tab if any
-    // // if (tabsData.length > 0) {
-    // //     var lastTab = tabsData[tabsData.length - 1];
-    // //     openTab(lastTab.notepadID, lastTab.tabID);
-    // // } else {
-    // //     openedTab = null;
-    // //     openedNotepad = null;
-    // // }
-
-    // openTab(openedNotepad, openedTab);
+    notebookSection?.appendChild(notebook_);
 }
 
+
+function openNotebook(notebook_id) {
+    openedNotebook = notebook_id;
+
+}
 
 
 if(JSON.parse(localStorage.getItem("savedTabs")) == null)
